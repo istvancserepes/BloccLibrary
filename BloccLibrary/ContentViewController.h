efaa07ff7c2b0aed44ead385b0d83c1ef35ebb09
Istvan Cserepes <istvan.cserepe@mmsone.com>
2015-06-03 12:55:53 +0200

    no message

---
+++ BloccLibrary/ContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContentViewController.h
+//  BloccLibrary
+//
+//  Created by Istvan Cserepes on 03/06/15.
+//  Copyright (c) 2015 Istvan Cserepes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentViewController : UIViewController
+
+@end
